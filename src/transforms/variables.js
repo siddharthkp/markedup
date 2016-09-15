c0e679ba8f46d6420d5923e1489c9a2409871647
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-09-16 02:45:39 +0530

    Only replacing if the variable exists

--- src/transforms/variables.js
+++ src/transforms/variables.js
@@ -1,13 +1,13 @@
-let variables = (content, data) => {
+let transform = (content, data) => {
     let re = /<(.*?)>/g;
     let matches = content.match(re);
 
     if (!matches) return content;
     for (let match of matches) {
         let variable = match.substring(1).substring(0, match.length - 2);
-        content = content.replace(match, data[variable]);
+        if (data[variable]) content = content.replace(match, data[variable]);
     }
     return content;
 };
 
-module.exports = variables;
+module.exports = transform;
