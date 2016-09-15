e30a39a801c0ab80f5d57d6ae439c9ea2bffc4a5
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-09-16 00:52:57 +0530

    Replaced function with =>

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ const marked = require('marked');
 const transforms = require('./transforms');
 
 const dang = (filePath, options, callback) => {
-    fs.readFile(filePath, function (err, content) {
+    fs.readFile(filePath, (err, content) => {
         if (err) return callback(new Error(err));
         content = content.toString();
         for (let transform of transforms) content = transform(content, options);
         let rendered = marked(content);
         return callback(null, rendered);
-  });
+    });
 };
 
 module.exports = dang;
