710f5f4d4cb92994a20b92d5d2a60cab95fffe45
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-09-16 05:52:01 +0530

    Correct implementation of comments

--- src/transforms/single-line-comments.js
+++ src/transforms/single-line-comments.js
@@ -2,11 +2,15 @@ const htmlTags = require('html-tags');
 const getData = require('../helpers/get-data');
 
 let transform = (content, data) => {
-    let re = /\/\/(.*$)/g;
+    let re = /\/\/(.*)/g;
     let matches = content.match(re);
 
     if (!matches) return content;
-    for (let match of matches) content = content.replace(match, '');
+    for (let match of matches) {
+        let htmlComments = match.replace('//', '<!--');
+        htmlComments = htmlComments + ' -->';
+        content = content.replace(match, htmlComments);
+    }
 
     return content;
 };
